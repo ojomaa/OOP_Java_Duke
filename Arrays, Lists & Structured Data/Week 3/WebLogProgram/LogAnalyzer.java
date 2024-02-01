a8ee138de99b6a0d6bb2ef1e7f53641f6f73fab5
ojomaa <omarjomaa21@gmail.com>
2024-02-01 11:35:15 -0500

    finish Finding Unique IP Addresses

--- Arrays, Lists & Structured Data/Week 3/WebLogProgram/LogAnalyzer.java
+++ Arrays, Lists & Structured Data/Week 3/WebLogProgram/LogAnalyzer.java
@@ -36,11 +36,47 @@ public class LogAnalyzer
         
         for(LogEntry le : records){
             String IpAdd = le.getIpAddress();
-            if(Ips.contains(IpAdd)){
+            if(!Ips.contains(IpAdd)){
                 Ips.add(IpAdd);
             }
         }
         
         return Ips.size();
+    }
+    
+    public void printAllHigherThanNum(int num){
+        for(LogEntry le : records){
+            int stat = le.getStatusCode();
+            if(stat > num){
+                System.out.println(le);
+            }
+        }
+    }
+    
+    public ArrayList<String> uniqueIpVisitsOnDay(String someday){
+        ArrayList<String> uniqueVisits = new ArrayList<String>();
+        for( LogEntry le : records){
+            String time = le.getAccessTime().toString();
+            String IpAdd = le.getIpAddress();
+            if(time.contains(someday) && !uniqueVisits.contains(IpAdd)){
+                uniqueVisits.add(IpAdd);
+            }
+        }
+        
+        return uniqueVisits;
+    }
+    
+    public int countUniqueIPsInRange(int low, int high) {
+        int count = 0;
+        ArrayList<String> uniqueIPs = new ArrayList<String>();
+        for(LogEntry le : records) {
+            int code = le.getStatusCode();
+            String ipAddr = le.getIpAddress();
+            if((code >= low && code <= high) && 
+            !uniqueIPs.contains(ipAddr)) {
+                uniqueIPs.add(ipAddr);
+            }
+        }
+        return uniqueIPs.size();
     }
 }
--- Arrays, Lists & Structured Data/Week 3/WebLogProgram/Tester.java
+++ Arrays, Lists & Structured Data/Week 3/WebLogProgram/Tester.java
@@ -22,4 +22,48 @@ public class Tester
         la.readFile("short-test_log.txt");
         la.printAll();
     }
+    
+    public void testUniqueIP() {
+        LogAnalyzer analyzer = new LogAnalyzer();
+        analyzer.readFile("short-test_log.txt");
+
+        int uniqueIPs = analyzer.countUniqueIps();
+        System.out.println("Number of unique IPs: " + uniqueIPs);
+    }
+    
+    public void testPrintAllHigherThanNum() {
+        LogAnalyzer analyzer = new LogAnalyzer();
+        analyzer.readFile("short-test_log");
+
+        System.out.println("Logs with status code higher than 300:");
+        analyzer.printAllHigherThanNum(300);
+    }
+    
+    public void testUniqueIpVisitsOnDay() {
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+        logAnalyzer.readFile("weblog-short_log");
+
+        String testDate = "Sep 14";
+
+        ArrayList<String> uniqueVisits = logAnalyzer.uniqueIpVisitsOnDay(testDate);
+
+        System.out.println("Unique IP visits on " + testDate + ":");
+        for (String ip : uniqueVisits) {
+            System.out.println(ip);
+        }
+    }
+    
+    public void testCountUniqueIPsInRange() {
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+        logAnalyzer.readFile("short-test_log");
+
+        int low = 200;
+        int high = 299;
+
+        int uniqueIPsInRange = logAnalyzer.countUniqueIPsInRange(low, high);
+
+        System.out.println("Number of unique IPs in range " + low + " to " + high + ": " + uniqueIPsInRange);
+    }
 }
