51e5d5244287195ee8f8f3705f06fd0d908eb6e5
ojomaa <omarjomaa21@gmail.com>
2024-01-31 23:06:56 -0500

    week 3 - add countUniqueIps method

--- Arrays, Lists & Structured Data/Week 3/WebLogProgram/LogAnalyzer.java
+++ Arrays, Lists & Structured Data/Week 3/WebLogProgram/LogAnalyzer.java
@@ -11,25 +11,36 @@ import edu.duke.*;
 
 public class LogAnalyzer
 {
-     private ArrayList<LogEntry> records;
+    private ArrayList<LogEntry> records;
      
-     public LogAnalyzer() {
+    public LogAnalyzer() {
          records = new ArrayList<>();
-     }
+    }
         
-     public void readFile(String filename) {
+    public void readFile(String filename) {
          FileResource fr = new FileResource(filename);
          for(String line : fr.lines()){
              LogEntry log = WebLogParser.parseEntry(line);
              records.add(log);
             }
-     }
+    }
         
-     public void printAll() {
+    public void printAll() {
          for (LogEntry le : records) {
              System.out.println(le);
          }
-     }
+    }
      
-     
+    public int countUniqueIps(){
+        ArrayList<String> Ips = new ArrayList<String>();
+        
+        for(LogEntry le : records){
+            String IpAdd = le.getIpAddress();
+            if(Ips.contains(IpAdd)){
+                Ips.add(IpAdd);
+            }
+        }
+        
+        return Ips.size();
+    }
 }
